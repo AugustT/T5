648f02b23dad087951dcfe87f82f81df1b2a49e2
PatWright <40758093+PatWright@users.noreply.github.com>
2019-11-14 14:09:02 +0000

    Update Tile_mapping.R

--- Tile_mapping.R
+++ Tile_mapping.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 library(leaflet)
 library(rgdal)
 
@@ -17,24 +16,4 @@ leaflet() %>%
   addTiles(urlTemplate = "http://walkerke.github.io/tiles/snow/{z}/{x}/{y}.png",
            attribution = 'Data source: <a href="http://guides.library.yale.edu/gisworkshoparchive">Yale University Library</a>', 
            options = tileOptions(minZoom = 15, maxZoom = 18, tms = TRUE))
-=======
-library(leaflet)
-library(rgdal)
 
-# code from https://rpubs.com/walkerke/custom_tiles
-leaflet() %>%
-  #setView(lng = 1615949, lat = 6313990, zoom = 5) %>%
-  setView(lng = -99, lat = 45, zoom = 5)%>%
-  #setView(lng = 0, lat = 7000000, zoom = 2) %>%
-  addTiles(urlTemplate = "https://patwright.github.io/Bin_T4/{z}/{x}/{y}.png",
-           #attribution = 'Data source: <a href="http://guides.library.yale.edu/gisworkshoparchive">Yale University Library</a>', 
-           options = tileOptions(minZoom = 5, maxZoom = 8, tms = TRUE))
-
-
-
-leaflet() %>%
-  setView(-0.1354223, 51.5135085, zoom = 17) %>%
-  addTiles(urlTemplate = "http://walkerke.github.io/tiles/snow/{z}/{x}/{y}.png",
-           attribution = 'Data source: <a href="http://guides.library.yale.edu/gisworkshoparchive">Yale University Library</a>', 
-           options = tileOptions(minZoom = 15, maxZoom = 18, tms = TRUE))
->>>>>>> 'Bin_T4'
